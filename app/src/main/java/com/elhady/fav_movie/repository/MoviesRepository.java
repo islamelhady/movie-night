686c4d64cf7c6ff31dbd4475672e2f0d2e7b8bbb
islamelhady <i.elhady44@gmail.com>
2020-06-28 09:31:36 +0200

    add MoviesRepository

---
+++ app/src/main/java/com/elhady/fav_movie/repository/MoviesRepository.java
@@ -0,0 +1,63 @@
+package com.elhady.fav_movie.repository;
+
+import androidx.annotation.NonNull;
+
+import com.elhady.fav_movie.iterface.OnGetMoviesCallback;
+import com.elhady.fav_movie.iterface.TMDbApi;
+import com.elhady.fav_movie.model.MoviesResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MoviesRepository {
+    private static final String BASE_URL = "https://api.themoviedb.org/3/";
+    private static final String LANGUAGE = "en-US";
+
+    private static MoviesRepository repository;
+
+    private TMDbApi api;
+
+    private MoviesRepository(TMDbApi api) {
+        this.api = api;
+    }
+
+    public static MoviesRepository getInstance() {
+        if (repository == null) {
+            Retrofit retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+            repository = new MoviesRepository(retrofit.create(TMDbApi.class));
+        }
+
+        return repository;
+    }
+
+    public void getMovies(final OnGetMoviesCallback callback) {
+        api.getPopularMovies("<YOUR_API_KEY>", LANGUAGE, 1)
+                .enqueue(new Callback<MoviesResponse>() {
+                    @Override
+                    public void onResponse(@NonNull Call<MoviesResponse> call, @NonNull Response<MoviesResponse> response) {
+                        if (response.isSuccessful()) {
+                            MoviesResponse moviesResponse = response.body();
+                            if (moviesResponse != null && moviesResponse.getMovies() != null) {
+                                callback.onSuccess(moviesResponse.getMovies());
+                            } else {
+                                callback.onError();
+                            }
+                        } else {
+                            callback.onError();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<MoviesResponse> call, Throwable t) {
+                        callback.onError();
+                    }
+                });
+    }
+}
