b64018ea91acdaa6c4bde948e727b249bb475682
islamelhady <i.elhady44@gmail.com>
2020-06-28 11:58:22 +0200

    edit TMDbApi interface

--- app/src/main/java/com/elhady/fav_movie/iterface/TMDbApi.java
+++ app/src/main/java/com/elhady/fav_movie/iterface/TMDbApi.java
@@ -1,5 +1,6 @@
 package com.elhady.fav_movie.iterface;
 
+import com.elhady.fav_movie.model.GenresResponse;
 import com.elhady.fav_movie.model.MoviesResponse;
 
 import retrofit2.Call;
@@ -11,4 +12,8 @@ public interface TMDbApi {
     Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey,
                                           @Query("language") String language,
                                           @Query("page") int page);
+
+    @GET("genre/movie/list")
+    Call<GenresResponse> getGenres(@Query("api_key") String apiKey,
+                                   @Query("language") String language);
 }
