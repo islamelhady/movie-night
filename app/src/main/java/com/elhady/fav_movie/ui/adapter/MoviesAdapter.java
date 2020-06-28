cac7a5b96f4ce5eb88d4d3b3928183a4c7ce1df9
islamelhady <i.elhady44@gmail.com>
2020-06-28 09:35:44 +0200

    edit MoviesAdapter

--- app/src/main/java/com/elhady/fav_movie/ui/adapter/MoviesAdapter.java
+++ app/src/main/java/com/elhady/fav_movie/ui/adapter/MoviesAdapter.java
@@ -3,12 +3,22 @@ package com.elhady.fav_movie.ui.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.elhady.fav_movie.R;
+import com.elhady.fav_movie.model.Movie;
+
+import java.util.List;
 
 public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewHolder> {
+
+    private List<Movie> movies;
+
+    public MoviesAdapter(List<Movie> movies) {
+        this.movies = movies;
+    }
 
     @Override
     public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -18,17 +28,33 @@ public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MovieViewH
 
     @Override
     public void onBindViewHolder(MovieViewHolder holder, int position) {
-        // TODO: Populate adapter with movies
+        holder.bind(movies.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 50;
+        return movies.size();
     }
 
     class MovieViewHolder extends RecyclerView.ViewHolder {
+        TextView releaseDate;
+        TextView title;
+        TextView rating;
+        TextView genres;
+
         public MovieViewHolder(View itemView) {
             super(itemView);
+            releaseDate = itemView.findViewById(R.id.item_movie_release_date);
+            title = itemView.findViewById(R.id.item_movie_title);
+            rating = itemView.findViewById(R.id.item_movie_rating);
+            genres = itemView.findViewById(R.id.item_movie_genre);
+        }
+
+        public void bind(Movie movie) {
+            releaseDate.setText(movie.getReleaseDate().split("-")[0]);
+            title.setText(movie.getTitle());
+            rating.setText(String.valueOf(movie.getRating()));
+            genres.setText("");
         }
     }
 }
