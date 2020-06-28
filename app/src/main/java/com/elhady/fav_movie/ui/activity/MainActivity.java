06471834e4070171adf4eef325938122b91475cd
islamelhady <i.elhady44@gmail.com>
2020-06-28 10:39:50 +0200

    edit MainActivity

--- app/src/main/java/com/elhady/fav_movie/ui/activity/MainActivity.java
+++ app/src/main/java/com/elhady/fav_movie/ui/activity/MainActivity.java
@@ -1,17 +1,48 @@
 package com.elhady.fav_movie.ui.activity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.elhady.fav_movie.R;
+import com.elhady.fav_movie.iterface.OnGetMoviesCallback;
+import com.elhady.fav_movie.model.Movie;
+import com.elhady.fav_movie.repository.MoviesRepository;
+import com.elhady.fav_movie.ui.adapter.MoviesAdapter;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView moviesList;
+    private MoviesAdapter adapter;
+
+    private MoviesRepository moviesRepository;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moviesRepository = MoviesRepository.getInstance();
+
+        moviesList = findViewById(R.id.movies_list);
+        moviesList.setLayoutManager(new LinearLayoutManager(this));
+
+        moviesRepository.getMovies(new OnGetMoviesCallback() {
+            @Override
+            public void onSuccess(List<Movie> movies) {
+                adapter = new MoviesAdapter(movies);
+                moviesList.setAdapter(adapter);
+            }
+
+            @Override
+            public void onError() {
+                Toast.makeText(MainActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
