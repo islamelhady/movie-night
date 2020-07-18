ebf40e7b988b03761791bec1be0390e012956c8d
islamelhady <i.elhady44@gmail.com>
2020-07-18 20:51:44 +0200

    edit activity movie

--- app/src/main/java/com/elhady/fav_movie/ui/activity/MovieActivity.java
+++ app/src/main/java/com/elhady/fav_movie/ui/activity/MovieActivity.java
@@ -1,16 +1,141 @@
 package com.elhady.fav_movie.ui.activity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.elhady.fav_movie.R;
+import com.elhady.fav_movie.iterface.OnGetGenresCallback;
+import com.elhady.fav_movie.iterface.OnGetMovieCallback;
+import com.elhady.fav_movie.model.Genre;
+import com.elhady.fav_movie.model.Movie;
+import com.elhady.fav_movie.repository.MoviesRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieActivity extends AppCompatActivity {
+    public static String MOVIE_ID = "movie_id";
+
+    private static String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/w780";
+    private static String YOUTUBE_VIDEO_URL = "http://www.youtube.com/watch?v=%s";
+    private static String YOUTUBE_THUMBNAIL_URL = "http://img.youtube.com/vi/%s/0.jpg";
+
+    private ImageView movieBackdrop;
+    private TextView movieTitle;
+    private TextView movieGenres;
+    private TextView movieOverview;
+    private TextView movieOverviewLabel;
+    private TextView movieReleaseDate;
+    private RatingBar movieRating;
+    private LinearLayout movieTrailers;
+    private LinearLayout movieReviews;
+
+    private MoviesRepository moviesRepository;
+    private int movieId;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
+
+        movieId = getIntent().getIntExtra(MOVIE_ID, movieId);
+
+        moviesRepository = MoviesRepository.getInstance();
+
+        setupToolbar();
+
+        initUI();
+
+        getMovie();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void initUI() {
+        movieBackdrop = findViewById(R.id.movieDetailsBackdrop);
+        movieTitle = findViewById(R.id.movieDetailsTitle);
+        movieGenres = findViewById(R.id.movieDetailsGenres);
+        movieOverview = findViewById(R.id.movieDetailsOverview);
+        movieOverviewLabel = findViewById(R.id.summaryLabel);
+        movieReleaseDate = findViewById(R.id.movieDetailsReleaseDate);
+        movieRating = findViewById(R.id.movieDetailsRating);
+        movieTrailers = findViewById(R.id.movieTrailers);
+        movieReviews = findViewById(R.id.movieReviews);
+    }
+
+    private void getMovie() {
+        moviesRepository.getMovie(movieId, new OnGetMovieCallback() {
+            @Override
+            public void onSuccess(Movie movie) {
+                movieTitle.setText(movie.getTitle());
+                movieOverviewLabel.setVisibility(View.VISIBLE);
+                movieOverview.setText(movie.getOverview());
+                movieRating.setVisibility(View.VISIBLE);
+                movieRating.setRating(movie.getRating() / 2);
+                getGenres(movie);
+                movieReleaseDate.setText(movie.getReleaseDate());
+                if (!isFinishing()) {
+                    Glide.with(MovieActivity.this)
+                            .load(IMAGE_BASE_URL + movie.getBackdrop())
+                            .apply(RequestOptions.placeholderOf(R.color.colorPrimary))
+                            .into(movieBackdrop);
+                }
+            }
+
+            @Override
+            public void onError() {
+                finish();
+            }
+        });
+    }
+
+    private void getGenres(final Movie movie) {
+        moviesRepository.getGenres(new OnGetGenresCallback() {
+            @Override
+            public void onSuccess(List<Genre> genres) {
+                if (movie.getGenres() != null) {
+                    List<String> currentGenres = new ArrayList<>();
+                    for (Genre genre : movie.getGenres()) {
+                        currentGenres.add(genre.getName());
+                    }
+                    movieGenres.setText(TextUtils.join(", ", currentGenres));
+                }
+            }
+
+            @Override
+            public void onError() {
+                showError();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    private void showError() {
+        Toast.makeText(MovieActivity.this, "Please check your internet connection.", Toast.LENGTH_SHORT).show();
     }
 }
