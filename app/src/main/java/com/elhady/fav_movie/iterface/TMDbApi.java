6188e3c1deaa7d9e903cab112a112fdbf8d7d953
islamelhady <i.elhady44@gmail.com>
2020-07-21 19:14:20 +0200

    add  getTrailer interface

--- app/src/main/java/com/elhady/fav_movie/iterface/TMDbApi.java
+++ app/src/main/java/com/elhady/fav_movie/iterface/TMDbApi.java
@@ -3,6 +3,7 @@ package com.elhady.fav_movie.iterface;
 import com.elhady.fav_movie.model.GenresResponse;
 import com.elhady.fav_movie.model.Movie;
 import com.elhady.fav_movie.model.MoviesResponse;
+import com.elhady.fav_movie.model.TrailerResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -23,19 +24,22 @@ public interface TMDbApi {
     @GET("movie/top_rated")
     Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey,
                                            @Query("language") String language,
-                                           @Query("page") int page
-    );
+                                           @Query("page") int page);
 
     @GET("movie/upcoming")
     Call<MoviesResponse> getUpcomingMovies(@Query("api_key") String apiKey,
                                            @Query("language") String language,
-                                           @Query("page") int page
-    );
+                                           @Query("page") int page);
 
     @GET("movie/{movie_id}")
     Call<Movie> getMovie(@Path("movie_id") int id,
                          @Query("api_key") String apiKEy,
                          @Query("language") String language);
+
+    @GET("movie/{movie_id}/videos")
+    Call<TrailerResponse> getTrailers(@Path("movie_id") int id,
+                                      @Query("api_key") String apiKEy,
+                                      @Query("language") String language);
 
 
 }
