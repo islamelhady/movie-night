26a36517be1b9820e1b004f4a3495d2b7498066a
islamelhady <i.elhady44@gmail.com>
2020-10-01 21:12:48 +0200

    Add home adapter

---
+++ app/src/main/java/com/elhady/fav_movie/adapter/HomeAdapter.java
@@ -0,0 +1,64 @@
+package com.elhady.fav_movie.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.elhady.fav_movie.Utils.Constants;
+import com.elhady.fav_movie.databinding.HomeItemBinding;
+import com.elhady.fav_movie.model.Movie;
+
+import java.util.ArrayList;
+
+public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.HomeViewHolder> {
+
+    private HomeItemBinding binding;
+    private ArrayList<Movie> moviesList;
+    private Context context;
+
+    public HomeAdapter(ArrayList<Movie> moviesList, Context context) {
+        this.moviesList = moviesList;
+        this.context = context;
+    }
+
+    public void setMoviesList(ArrayList<Movie> moviesList) {
+        this.moviesList = moviesList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public HomeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        binding = HomeItemBinding.inflate(inflater, parent, false);
+        return new HomeViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull HomeViewHolder holder, int position) {
+        holder.binding.movieItemName.setText(moviesList.get(position).getTitle());
+        holder.binding.movieItemVotes.setText(moviesList.get(position).getVote_count()+"");
+
+        Glide.with(context).load(Constants.ImageBaseURLw500 + moviesList.get(position).getPoster_path())
+                .into(holder.binding.movieItemImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class HomeViewHolder extends RecyclerView.ViewHolder {
+
+        private HomeItemBinding binding;
+
+        public HomeViewHolder(HomeItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+}
