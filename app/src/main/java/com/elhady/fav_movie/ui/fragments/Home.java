fdc9eeffa0013d6916092c19e3e5cad8122f1d19
islamelhady <i.elhady44@gmail.com>
2020-10-09 21:56:36 +0200

    Add set onclick to open fragment of movies

--- app/src/main/java/com/elhady/fav_movie/ui/fragments/Home.java
+++ app/src/main/java/com/elhady/fav_movie/ui/fragments/Home.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -50,7 +51,6 @@ public class Home extends Fragment {
 
         viewModel = new ViewModelProvider(Home.this).get(HomeViewModel.class);
 
-//        upcomingMovies = new ArrayList<>();
 
         map.put("api_key", Constants.API_KEY);
         map.put("page","1");
@@ -58,6 +58,7 @@ public class Home extends Fragment {
         observeData();
         getMoviesList();
         setUpRecyclerViewsAndViewPager();
+        setUpOnclick();
     }
 
 
@@ -113,5 +114,40 @@ public class Home extends Fragment {
         binding.popularRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL,false));
         popularAdapter = new HomeAdapter(getContext(),popularMovies);
         binding.popularRecyclerView.setAdapter(popularAdapter);
+    }
+
+    private void setUpOnclick(){
+        binding.viewAllCurrent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeDirections.ActionHome2ToMovies action = HomeDirections.actionHome2ToMovies();
+                action.setMovieCategory(Constants.Current);
+                Navigation.findNavController(v).navigate(action);
+            }
+        });
+        binding.viewAllUpcoming.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeDirections.ActionHome2ToMovies action = HomeDirections.actionHome2ToMovies();
+                action.setMovieCategory(Constants.Upcoming);
+                Navigation.findNavController(v).navigate(action);
+            }
+        });
+        binding.viewAllPopular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeDirections.ActionHome2ToMovies action = HomeDirections.actionHome2ToMovies();
+                action.setMovieCategory(Constants.Popular);
+                Navigation.findNavController(v).navigate(action);
+            }
+        });
+        binding.viewAllTopRated.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HomeDirections.ActionHome2ToMovies action = HomeDirections.actionHome2ToMovies();
+                action.setMovieCategory(Constants.TopRated);
+                Navigation.findNavController(v).navigate(action);
+            }
+        });
     }
 }
