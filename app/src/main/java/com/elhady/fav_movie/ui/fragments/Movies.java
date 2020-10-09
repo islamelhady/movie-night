3b4c7dfff93da49e2fbb02d9c9fd42c324fb3a1b
islamelhady <i.elhady44@gmail.com>
2020-10-09 12:12:00 +0200

    Add data at movies fragment

--- app/src/main/java/com/elhady/fav_movie/ui/fragments/Movies.java
+++ app/src/main/java/com/elhady/fav_movie/ui/fragments/Movies.java
@@ -2,65 +2,125 @@ package com.elhady.fav_movie.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.elhady.fav_movie.R;
+import com.elhady.fav_movie.Utils.Constants;
+import com.elhady.fav_movie.adapter.CategoryMoviesAdapter;
+import com.elhady.fav_movie.databinding.MoviesLayoutBinding;
+import com.elhady.fav_movie.model.Movie;
+import com.elhady.fav_movie.viewmodel.HomeViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Movies#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Movies extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MoviesLayoutBinding binding;
+    private HomeViewModel viewModel;
+    private HashMap<String, String> map;
+    private String moviesCategory="";
+    private CategoryMoviesAdapter adapter;
+    private ArrayList<Movie> moviesList;
 
     public Movies() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Movies.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Movies newInstance(String param1, String param2) {
-        Movies fragment = new Movies();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = MoviesLayoutBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        map = new HashMap<>();
+        viewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        MoviesArgs args = MoviesArgs.fromBundle(getArguments());
+        moviesCategory = args.getMovieCategory();
+
+        map.put("api_key", Constants.API_KEY);
+        map.put("page","1");
+
+        initRecyclerView();
+        observeData();
+        getMoviesList();
+    }
+
+    private void initRecyclerView() {
+        binding.moviesRecyclerView.setLayoutManager(new GridLayoutManager(getContext(),3));
+        adapter = new CategoryMoviesAdapter(getContext(),moviesList);
+        binding.moviesRecyclerView.setAdapter(adapter);
+    }
+
+    private void observeData(){
+        switch (moviesCategory){
+            case Constants.Current:
+                viewModel.getCurrentlyShowingList().observe(getViewLifecycleOwner(), new Observer<ArrayList<Movie>>() {
+                    @Override
+                    public void onChanged(ArrayList<Movie> movies) {
+                        adapter.setMovieList(movies);
+                    }
+                });
+                break;
+            case Constants.Upcoming:
+                viewModel.getUpcomingMoviesList().observe(getViewLifecycleOwner(), new Observer<ArrayList<Movie>>() {
+                    @Override
+                    public void onChanged(ArrayList<Movie> movies) {
+                        adapter.setMovieList(movies);
+                    }
+                });
+                break;
+            case Constants.TopRated:
+                viewModel.getTopRatedMoviesList().observe(getViewLifecycleOwner(), new Observer<ArrayList<Movie>>() {
+                    @Override
+                    public void onChanged(ArrayList<Movie> movies) {
+                        adapter.setMovieList(movies);
+                    }
+                });
+                break;
+            case Constants.Popular:
+                viewModel.getPopularMoviesList().observe(getViewLifecycleOwner(), new Observer<ArrayList<Movie>>() {
+                    @Override
+                    public void onChanged(ArrayList<Movie> movies) {
+                        adapter.setMovieList(movies);
+                    }
+                });
+                break;
         }
     }
 
+    private void getMoviesList(){
+        switch (moviesCategory){
+            case Constants.Current:
+                viewModel.getCurrentlyShowingMovies(map);
+                break;
+            case Constants.Upcoming:
+                viewModel.getUpcomingMovies(map);
+                break;
+            case Constants.TopRated:
+                viewModel.getTopRatedMovies(map);
+                break;
+            case Constants.Popular:
+                viewModel.getPopularMovies(map);
+                break;
+        }
+    }
+
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.movies_layout, container, false);
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
