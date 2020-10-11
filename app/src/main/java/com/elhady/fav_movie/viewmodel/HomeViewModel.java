90130507861c3de09a66fdc528c094cad85a193b
islamelhady <i.elhady44@gmail.com>
2020-10-11 13:51:13 +0200

    Add method movie details in view model

--- app/src/main/java/com/elhady/fav_movie/viewmodel/HomeViewModel.java
+++ app/src/main/java/com/elhady/fav_movie/viewmodel/HomeViewModel.java
@@ -6,9 +6,16 @@ import androidx.hilt.lifecycle.ViewModelInject;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.elhady.fav_movie.model.Actor;
+import com.elhady.fav_movie.model.Cast;
+import com.elhady.fav_movie.model.Genre;
 import com.elhady.fav_movie.model.Movie;
 import com.elhady.fav_movie.model.MovieResponse;
 import com.elhady.fav_movie.repository.Repository;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -28,6 +35,9 @@ public class HomeViewModel extends ViewModel {
     private MutableLiveData<ArrayList<Movie>> popularMoviesList = new MutableLiveData<>();
     private MutableLiveData<ArrayList<Movie>> topRatedMoviesList = new MutableLiveData<>();
     private MutableLiveData<ArrayList<Movie>> upcomingMoviesList = new MutableLiveData<>();
+    private MutableLiveData<ArrayList<Cast>> movieCastList = new MutableLiveData<>();
+    private MutableLiveData<Movie> movieDetails = new MutableLiveData<>();
+    private MutableLiveData<Actor> actorDetails = new MutableLiveData<>();
 
     private final CompositeDisposable disposable = new CompositeDisposable();
 
@@ -50,6 +60,18 @@ public class HomeViewModel extends ViewModel {
 
     public MutableLiveData<ArrayList<Movie>> getUpcomingMoviesList(){
         return upcomingMoviesList;
+    }
+
+    public MutableLiveData<Movie> getMovie() {
+        return movieDetails;
+    }
+
+    public MutableLiveData<Actor> getActor() {
+        return actorDetails;
+    }
+
+    public MutableLiveData<ArrayList<Cast>> getMovieCastList() {
+        return movieCastList;
     }
 
 
@@ -106,6 +128,53 @@ public class HomeViewModel extends ViewModel {
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(result -> upcomingMoviesList.setValue(result.getResults()),
                         error -> Log.e(TAG, "getUpcoming: " + error.getMessage()))
+        );
+    }
+
+    public void getMovieDetails(int movieId, HashMap<String, String> map) {
+        disposable.add(repository.getMovieDetails(movieId, map)
+                .subscribeOn(Schedulers.io())
+                .map(new Function<Movie, Movie>() {
+                    @Override
+                    public Movie apply(Movie movie) throws Throwable {
+                        ArrayList<String> genreNames = new ArrayList<>();
+                        // MovieResponse gives list of genre(object) so we will map each id to it genre name here.a
+
+                        for(Genre genre : movie.getGenres()){
+                            genreNames.add(genre.getName());
+                        }
+                        movie.setGenre_names(genreNames);
+                        return movie;
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> movieDetails.setValue(result),
+                        error -> Log.e(TAG, "getMovieDetails: " + error.getMessage()))
+        );
+    }
+
+    public void getCast(int movieId, HashMap<String, String> map) {
+        disposable.add(repository.getCast(movieId, map)
+                .subscribeOn(Schedulers.io())
+                .map(new Function<JsonObject, ArrayList<Cast>>() {
+                    @Override
+                    public ArrayList<Cast> apply(JsonObject jsonObject) throws Throwable {
+                        JsonArray jsonArray = jsonObject.getAsJsonArray("cast");
+                        return  new Gson().fromJson(jsonArray.toString(), new TypeToken<ArrayList<Cast>>(){}.getType());
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> movieCastList.setValue(result),
+                        error -> Log.e(TAG, "getCastList: " + error.getMessage()))
+        );
+    }
+
+    public void getActorDetails(int personId, HashMap<String,String> map) {
+        disposable.add(repository.getActorDetails(personId, map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(result -> actorDetails.setValue(result),
+                        error -> Log.e(TAG, "getActorDetails: " + error.getMessage()))
         );
     }
 }
