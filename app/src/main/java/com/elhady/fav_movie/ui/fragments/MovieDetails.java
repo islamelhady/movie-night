a81e2a919623026084b29294e1b141c55b4beea2
islamelhady <i.elhady44@gmail.com>
2020-10-13 19:34:51 +0200

    add cast item

--- app/src/main/java/com/elhady/fav_movie/ui/fragments/MovieDetails.java
+++ app/src/main/java/com/elhady/fav_movie/ui/fragments/MovieDetails.java
@@ -2,65 +2,44 @@ package com.elhady.fav_movie.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.elhady.fav_movie.R;
+import com.elhady.fav_movie.databinding.MovieDetailsLayoutBinding;
+import com.elhady.fav_movie.model.Cast;
+import com.elhady.fav_movie.model.Movie;
+import com.elhady.fav_movie.viewmodel.HomeViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MovieDetails#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
 public class MovieDetails extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MovieDetailsLayoutBinding binding;
 
     public MovieDetails() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MovieDetails.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MovieDetails newInstance(String param1, String param2) {
-        MovieDetails fragment = new MovieDetails();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = MovieDetailsLayoutBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.movie_details_layout, container, false);
     }
 }
